41882ecd6091fcf4cdbad5d59e2e846da4ef20d2
nhilikeknee <56176648+nhilikeknee@users.noreply.github.com>
2022-02-27 22:21:39 -0800

    got delete function

--- assets/scripts/blog.js
+++ assets/scripts/blog.js
@@ -90,20 +90,48 @@ function showAllBlogs() {
         let tempTitle = document.createElement("h3");
         let tempDate = document.createElement("p");
         let tempSummary = document.createElement("p");
+        let editBtn = document.createElement("button");
+        let deleteBtn = document.createElement("button");
 
 
         tempTitle.innerHTML = titleArr[ind];
         tempDate.innerHTML = dateArr[ind];
-        tempSummary.innerText = summaryArr[ind];
+        tempSummary.innerHTML = summaryArr[ind];
+        editBtn.innerHTML = "edit";
+        deleteBtn.innerHTML = "delete";
+        deleteBtn.classList.add("deleteButtons");
+
 
 
         blog.appendChild(tempTitle);
         blog.appendChild(tempDate);
         blog.appendChild(tempSummary);
+        blog.appendChild(editBtn);
+        blog.appendChild(deleteBtn);
         blogSection.appendChild(blog);
     });
 
 
+    const allDeleteButtons = document.getElementsByClassName('deleteButtons');
+    console.log(allDeleteButtons);
 
+    if (allDeleteButtons != null) {
+        for (let i = 0; i < allDeleteButtons.length; i++) {
+            allDeleteButtons[i].addEventListener('click', function openDeleteModal() {
+                // console.log("OPEN DELETE MODAL with index: " + i);
+                const confirmation = confirm("Are you sure you want to delete " + titleArr[i] + " ?");
+                if (confirmation == true) {
+                    titleArr.splice(i, 1);
+                    dateArr.splice(i, 1);
+                    summaryArr.splice(i, 1);
+                    localStorage.setItem("titles", JSON.stringify(titleArr));
+                    localStorage.setItem("dates", JSON.stringify(dateArr));
+                    localStorage.setItem("summaries", JSON.stringify(summaryArr));
+                    location.reload();
+                }
+            }
+            );
+        }
+    }
 }
 showAllBlogs();
